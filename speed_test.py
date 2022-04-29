73e2724fb70025d8999f5fb837f959f095313829
EmptyDot <edvin.hermanson@gmail.com>
2022-04-29 17:17:00 +0200

    did a run of 100000

--- speed_test.py
+++ speed_test.py
@@ -1,20 +1,41 @@
 from timeit import timeit
 import numpy as np
+import curses
+from curses import wrapper
 
 
-def time_file(filename, number=1000000):
+def time_file(stdscr, filename, number=1000000):
     with open(filename, 'r') as f:
         content = f.read()
         times = []
-        for _ in range(number):
+        percent = 0
+        for i in range(number):
             times.append(timeit(stmt=content, number=1))
-        print(f'mean: {np.mean(times)}')
-        print(f'median: {np.median(times)}')
-        print(f'std: {np.std(times)}')
-        print(f'min: {np.min(times)}')
-        print(f'max: {np.max(times)}')
 
+            if i % (number/100) == 0:
+                stdscr.clear()
+                percent += 1
+                stdscr.addstr(0, 0, f'{percent}%')
+                stdscr.refresh()
+        stdscr.clear()
+        stdscr.addstr(0, 0, f'number: {number}\n'
+                            f'total: {sum(times)}\n'
+                            f'mean: {np.mean(times)}\n'
+                            f'median: {np.median(times)}\n'
+                            f'std: {np.std(times)}\n'
+                            f'min: {np.min(times)}\n'
+                            f'max: {np.max(times)}')
+        stdscr.refresh()
+        stdscr.getch()
 
-time_file('sudoku_for_speed_test.py', number=100)
+wrapper(time_file, filename='sudoku_for_speed_test.py', number=100000)
 
-
+"""
+number: 100000
+total: 7989.790536999913
+mean: 0.07989790536999912
+median: 0.025036000000000058
+std: 0.2406041492989288
+min: 0.002986699999837583
+max: 23.329247399999986
+"""
