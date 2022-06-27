c0f506345ce1314334f7f32105e9549f73054a77
EmptyDot <edvin.hermanson@gmail.com>
2022-06-27 17:34:23 +0200

    prep for merge

--- speed_test.py
+++ speed_test.py
@@ -1,10 +1,9 @@
 from timeit import timeit
 import numpy as np
-import curses
-from curses import wrapper
 
 
-def time_file(stdscr, filename, number=1000000):
+
+def time_file(filename, number=1000000):
     with open(filename, 'r') as f:
         content = f.read()
         times = []
@@ -13,32 +12,19 @@ def time_file(stdscr, filename, number=1000000):
             times.append(timeit(stmt=content, number=1))
 
             if i % (number/100) == 0:
-                stdscr.clear()
                 percent += 1
-                stdscr.addstr(0, 0, f'{percent}%')
-                stdscr.refresh()
-        stdscr.clear()
-        stdscr.addstr(0, 0, f'number: {number}\n'
+                print(f'{percent}%')
+
+        print(0, 0, f'number: {number}\n'
                             f'total: {sum(times)}\n'
                             f'mean: {np.mean(times)}\n'
                             f'median: {np.median(times)}\n'
                             f'std: {np.std(times)}\n'
                             f'min: {np.min(times)}\n'
                             f'max: {np.max(times)}')
-        stdscr.refresh()
-        stdscr.getch()
 
 # wrapper(time_file, filename='sudoku_for_speed_test.py', number=100000)
 
-"""
-number: 100000
-total: 7989.790536999913
-mean: 0.07989790536999912
-median: 0.025036000000000058
-std: 0.2406041492989288
-min: 0.002986699999837583
-max: 23.329247399999986
-"""
 
 def compare_two(a: str, b: str, setup='pass'):
     ta = timeit(a, setup)
